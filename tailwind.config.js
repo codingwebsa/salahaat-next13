3a796685dfa2e430589791ab4db720a12c7ff362
codingwebsa <codingwebsa@gmail.com>
2023-03-10 13:22:56 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  darkMode: "class",
+  content: [
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        hind: ["var(--font-hind)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
